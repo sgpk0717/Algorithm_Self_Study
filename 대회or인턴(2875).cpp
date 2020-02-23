60d13336be7f020394f10c454868d94b752307a5
sgpk0717 <38274677+sgpk0717@users.noreply.github.com>
2020-02-23 13:54:39 +0900

    Add files via upload

---
+++ 대회or인턴(2875).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int Answer;
+
+int main(void)
+{
+	int N, M, K;
+	cin >> N >> M >> K;
+	
+	while(K!=0) {	
+		if(N!=0 && N>=2*M) {
+			N-=1;
+		}
+		else if(M!=0 && N<2*M) {
+			M-=1;
+		}
+		--K;
+	}
+	
+	if(N<2 || M==0) {
+		cout << 0 << endl;
+		return 0;
+	}
+	
+	while(M!=0) {
+		if(N>=2) {	
+			++Answer;
+			N-=2;
+			M-=1;
+		}
+		else
+			break;
+	}
+	
+	cout << Answer << endl;
+	
+	
+	
+	return 0;
+}
