f11110709642169b72c4a339cd1d747a5697bea0
Seong-Guk Park <38274677+sgpk0717@users.noreply.github.com>
2020-12-09 00:03:13 +0900

    Update [백준] 2xn 타일링 2(11727).py

--- [백준] 2xn 타일링 2(11727).py
+++ [백준] 2xn 타일링 2(11727).py
@@ -1,7 +1,10 @@
 N = int(input())
-dp = [0 for _ in range(N)]
-dp[0] = 1
-dp[1] = 3
-for i in range(2,N):
-    dp[i] = (dp[i-2]*2 + dp[i-1])%10007
-print(dp[-1])
+if N==1:
+    print(1)
+else:
+    dp = [0 for _ in range(N)]
+    dp[0] = 1
+    dp[1] = 3
+    for i in range(2,N):
+        dp[i] = (dp[i-2]*2 + dp[i-1])%10007
+    print(dp[-1])
