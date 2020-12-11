2d5f97eea1a4657001f7797df99ed3d5970d97ac
Seong-Guk Park <38274677+sgpk0717@users.noreply.github.com>
2020-12-11 22:45:09 +0900

    Add files via upload

---
+++ [백준] 카드 구매하기(11052).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> p(10001);
+int dp[1001];
+
+int main(void){
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cin >> p[i];
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            dp[i] = max(dp[i],dp[i-j]+p[j]);
+        }
+    }
+    cout << dp[n];
+    
+    return 0;
+}
