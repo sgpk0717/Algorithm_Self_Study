6b5fca201b6693df34989ccb904550174e9f47b6
Seong-Guk Park <38274677+sgpk0717@users.noreply.github.com>
2020-12-12 20:24:42 +0900

    Update [백준] 카드 구매하기(11052).cpp

--- [백준] 카드 구매하기(11052).cpp
+++ [백준] 카드 구매하기(11052).cpp
@@ -12,7 +12,6 @@ int main(void){
     for(int i=1; i<=n; i++){
         cin >> p[i];
     }
-
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
             dp[i] = max(dp[i],dp[i-j]+p[j]);
