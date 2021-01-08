44bdf4e5f1b07cf55036dafc812f71c924ce0816
Seong-Guk Park <38274677+sgpk0717@users.noreply.github.com>
2021-01-08 17:49:39 +0900

    Add files via upload

--- [백준] 별 찍기 - 17(10992).py
+++ [백준] 별 찍기 - 17(10992).py
@@ -1,20 +1,26 @@
 N = int(input())
 
-for i in range(N-1):
-    print(' ',end='')
-print('*')
-
-left_white = N-2
-center_white = 1
-for i in range(N-2):
-    for j in range(left_white):
-        print(' ',end='')
-    print('*', end='')
-    for j in range(center_white):
+if N == 1:
+    print('*')
+elif N == 2:
+    print(' *')
+    print('***')
+else:
+    for i in range(N-1):
         print(' ',end='')
     print('*')
-    left_white-=1
-    center_white+=2
-for i in range(2*N-1):
-    print('*', end='')
-print()
+
+    left_white = N-2
+    center_white = 1
+    for i in range(N-2):
+        for j in range(left_white):
+            print(' ',end='')
+        print('*', end='')
+        for j in range(center_white):
+            print(' ',end='')
+        print('*')
+        left_white-=1
+        center_white+=2
+    for i in range(2*N-1):
+        print('*', end='')
+    print()
