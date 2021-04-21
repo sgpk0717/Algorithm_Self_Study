b864a8dfc93b50aa46fafffd154c201f4deb13af
Seong-Guk Park <38274677+sgpk0717@users.noreply.github.com>
2021-04-21 18:51:17 +0900

    Add files via upload

--- [백준] 작업(2056).py
+++ [백준] 작업(2056).py
@@ -1,9 +1,10 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 dp = [0 for _ in range(N+1)]
 for i in range(N):
     info = list(map(int,input().split()))
-    max_pre_time = 0
-    for pre_op in info[2:]:
-        max_pre_time = max(max_pre_time, dp[pre_op])
+    max_pre_time = max(dp[pre_op] for pre_op in info[1:])
     dp[i+1] = max_pre_time + info[0]
 print(max(dp))
