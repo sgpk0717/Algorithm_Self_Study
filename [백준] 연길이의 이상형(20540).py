4bafdfdae62e355ea6f5b5232557bd3a14521fe8
Seong-Guk Park <38274677+sgpk0717@users.noreply.github.com>
2021-05-19 23:45:53 +0900

    .
    .

---
+++ [백준] 연길이의 이상형(20540).py
@@ -0,0 +1,6 @@
+idol = {'E': 'I', 'S': 'N', 'T': 'F', 'J': 'P', 'I': 'E', 'N': 'S', 'F': 'T', 'P': 'J'}
+
+for c in input():
+    print(idol[c],end='')
+
+    https: // www.acmicpc.net / problem / 20540
